9998e0fbecff87de60a737cfb9e4bc8d81c96b81
Rosangela Almeida <rosangelabatista.dev@gmail.com>
2024-09-27 12:21:51 -0300

    Removendo pasta out e atualizando .gitignore

--- src/interfaces/IBancoDeDados.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-public interface IBancoDeDados <T>{
-    boolean cadastrar(T entidade);
-    boolean alterar(T entidade);
-    T buscar(String valor);
-    boolean deletar(String valor);
-}
