b324592e14b6a451ce59c823429ee548bbb0ed7f
Gustavo Habib <gushabib@gmail.com>
2024-09-30 12:17:13 -0300

    alterações nas classes cliente PJ e PF do Gustavo Queiroz

--- src/dominio/Cliente.java
+++ src/dominio/Cliente.java
@@ -3,22 +3,31 @@ package dominio;
 import java.io.Serializable;
 
 public class Cliente implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     private String nome;
-    private String cpf;
     private String logradouro ;
 
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+
     public String getNome() {
         return nome;
     }
-    public void setNome(String nome) {
 
+    public void setNome(String nome) {
+        if (nome == null || nome.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nome não pode ser vazio ou nulo.");
+        }
+        this.nome = nome;
+    }
+
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
     }
 
     public static class ClientePessoaFisica extends Cliente {
--- src/dominio/ClientePessoaJuridica.java
+++ src/dominio/ClientePessoaJuridica.java
@@ -1,4 +1,21 @@
 package dominio;
 
-public class ClientePessoaJuridica {
+public class ClientePessoaJuridica extends Cliente {
+
+    private String cnpj;
+
+    public ClientePessoaJuridica(String cnpj) {
+        this.setCnpj(cnpj);
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        if (cnpj == null || cnpj.length() != 14 || !cnpj.matches("\\d+")) {
+            throw new IllegalArgumentException("CNPJ inválido. O CNPJ deve conter 14 dígitos.");
+        }
+        this.cnpj = cnpj;
+    }
 }
